cefd2fab6da0fe9ca847328edee2d2ae8a5c58e5
Jason Christian H <nakamarujc@gmail.com>
2023-02-23 21:43:38 +0700

    Added liblouis to lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <Arduino.h>
+#include <liblouis.h>
 
 void setup() {
   // put your setup code here, to run once:
+  widechar inbuf[15] = {'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
+  int inbuflen = 15;
+  widechar outbuf[15];
+  int outbuflen = 15;
+  const char *table_list = {"en-us-g1.ctb"};
+  widechar outbackbuf[15];
+  widechar outbuftranslated[15];
+  lou_dotsToChar(table_list, outbuftranslated, outbackbuf, outbuflen, 0);
+  printf("Translated string back from dot: ");
+  for (int i = 0; i < outbuflen; i++)
+    Serial.print(outbackbuf[i]);
 }
 
 void loop() {
