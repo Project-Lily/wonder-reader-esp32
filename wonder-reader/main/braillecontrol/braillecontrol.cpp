1b906dd5d58ea0e70a5f459db68542d763a91dec
Jason Christian H <nakamarujc@gmail.com>
2023-02-23 09:48:17 +0700

    Added components

---
+++ wonder-reader/main/braillecontrol/braillecontrol.cpp
@@ -0,0 +1,4 @@
+
+int get_braille() {
+  return 5;
+}
--- wonder-reader/main/main.cpp
+++ wonder-reader/main/main.cpp
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
 
 void app_main(void)
 {
