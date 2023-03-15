fc337f97734175cac5d557f5fabc8433e45ed2c3
Jason Christian H <nakamarujc@gmail.com>
2023-03-15 12:24:01 +0700

    Moved network code to its own file

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,17 @@
 #include <Arduino.h>
 #include <liblouis.h>
 #include <SPIFFS.h>
-#include "wonderconfig.h"
-#include "control/braillecontrol.h"
-#include <mdns.h>
 #include "nvs_flash.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "esp_log.h"
-#include "esp_wifi.h"
 #include "esp_event.h"
-#include "esp_system.h"
-#include "esp_netif.h"
+#include "wonderconfig.h"
+#include "control/braillecontrol.h"
+#include "network.h"
 
 static const char* TAG = "main";
-
-void init_mdns() {
-  const char* hostname = "wreader-1";
-
-  // Start mDNS
-  ESP_ERROR_CHECK(mdns_init());
-  ESP_ERROR_CHECK(mdns_hostname_set(hostname));
-  ESP_ERROR_CHECK(mdns_instance_name_set("Wonder Reader 1"));
-  ESP_ERROR_CHECK(mdns_service_add(NULL, "_http", "_tcp", 80, NULL, 0));
-
-  ESP_LOGI(TAG, "mDNS Initialized. Hostname: %s", hostname);
-}
-
-void wifi_event_handler(void* arg, esp_event_base_t event_base,
-  int32_t event_id, void* event_data) {
-  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-    ESP_LOGI(TAG, "Connected to Wi-Fi!");
-    esp_wifi_connect();
-  } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-    ESP_LOGI(TAG, "Disconnected. Attempting to reconnect to Wi-Fi");
-    esp_wifi_connect();
-  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-    ip_event_got_ip_t* ip_event = (ip_event_got_ip_t*) event_data;
-    ESP_LOGI(TAG, "Got IP Address:" IPSTR, IP2STR(&ip_event->ip_info.ip));
-  }
-}
-
-void init_network() {
-  ESP_ERROR_CHECK(esp_netif_init());
-  esp_netif_create_default_wifi_sta();
-
-  // Register event handler
-  esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL);
-  esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL);
-
-  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-  
-  wifi_config_t wifi_config = {
-    .sta = {
-      {.ssid = "SSID"},
-      {.password = "PASSWORD"}
-    },
-  };
-
-  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-  ESP_ERROR_CHECK(esp_wifi_start());
-  ESP_LOGI(TAG, "Network initialized");
-}
 
 void setup() {
   ESP_LOGI(TAG, "Wonder Reader Hello!");
@@ -93,8 +40,8 @@ void setup() {
   // Initialize main loop
   ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-  init_network();
-  init_mdns();
+  wonder::init_network();
+  wonder::init_mdns();
 
   // wonder::init_motors();
 
---
+++ src/network.h
@@ -0,0 +1,9 @@
+#ifndef __NETWORK_H_
+#define __NETWORK_H_
+
+namespace wonder {
+  void init_mdns();
+  void init_network();
+}
+
+#endif
